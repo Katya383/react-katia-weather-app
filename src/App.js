8e8106b46360b98bcc2a48efc85a1e2e5b2f491f
Katya383 <82849906+Katya383@users.noreply.github.com>
2021-07-21 18:53:20 -0400

    Added Github URL

--- src/App.js
+++ src/App.js
@@ -3,7 +3,19 @@ import React from "react";
 import './App.css';
 
 export default function App() {
-  return <div className="App">Weather App</div>;
-      
-}
+  return (
+  <div className="App">
+    Weather App
+     <footer>
+     This project is coded by Katia Barnave and is
+    <a href="https://github.com/Katya383/react-katia-weather-app"
+    target="_blank"
+    rel="noreferrer"
+       >
+       {"  "}open-sourced on Github
+       </a>
+     </footer>
+    </div>
+);
+  } 
 
