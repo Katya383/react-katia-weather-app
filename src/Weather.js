91a29af5499dc27da4af1647637b76c7e2024014
Katya383 <82849906+Katya383@users.noreply.github.com>
2021-07-22 18:59:12 -0400

    Added additional styling

--- src/Weather.js
+++ src/Weather.js
@@ -1,22 +1,26 @@
 import React from "react";
 import "./Weather.css";
 
-
 export default function Weather() {
-  return(
+  return (
     <div className="Weather">
       <form>
         <div className="row">
           <div className="col-9">
-        <input type="search" 
-        placeholder="Enter a city.." 
-        Name="form-control" 
-        />
-        </div>
-        <div className="col-3">
-        <input type="submit" value="search" 
-        className ="btn btn-primary" />
-        </div>
+            <input
+              type="search"
+              placeholder="Enter a city.."
+              Name="form-control"
+              autoFocus="on"
+            />
+          </div>
+          <div className="col-3">
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-primary w-100"
+            />
+          </div>
         </div>
       </form>
       <h1>New York</h1>
@@ -24,12 +28,19 @@ export default function Weather() {
         <li>Wednesday 07:52</li>
         <li>Partly Cloudy</li>
       </ul>
-      <div className ="row">
+      <div className="row mt-3">
         <div className=" col-6">
-          <img src= "https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
-          alt= "Partly cloudy"
-          />
-          86°F
+          <div className="clearfix">
+            <img
+              src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+              alt="Partly cloudy"
+              className="float-left"
+            />
+            <div className="float-left">
+              <span className="temperature">86</span>
+              <span className="unit">°F</span>
+            </div>
+          </div>
         </div>
         <div className="col-6">
           <ul>
@@ -39,6 +50,6 @@ export default function Weather() {
           </ul>
         </div>
       </div>
-      </div>
+    </div>
   );
 }
