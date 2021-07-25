75bddf17434099d82384a8f23a38169db35a3acb
Katya383 <82849906+Katya383@users.noreply.github.com>
2021-07-25 11:20:52 -0400

    Added WeatherIcons-animated

--- src/Weather.js
+++ src/Weather.js
@@ -7,6 +7,7 @@ export default function Weather(props) {
   const [ready, setReady] = useState(false);
   const [city, setCity] = useState(props.defaultCity);
   const [weatherData, setWeatherData] = useState({});
+
   function handleResponse(response) {
     console.log(response.data);
     setWeatherData({
@@ -14,7 +15,7 @@ export default function Weather(props) {
       humidity: response.data.main.humidity,
       date: new Date(response.data.dt * 1000),
       description: response.data.weather[0].description,
-      iconUrl: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+      icon: response.data.weather[0].icon,
       wind: response.data.wind.speed,
       city: response.data.name,
     });
--- src/WeatherDetails.js
+++ src/WeatherDetails.js
@@ -1,5 +1,6 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
+import WeatherIcon from "./WeatherIcon";
 
 export default function WeatherDetails(props) {
   return (
@@ -14,11 +15,9 @@ export default function WeatherDetails(props) {
       <div className="row mt-3">
         <div className=" col-6">
           <div className="clearfix">
-            <img
-              src={props.data.iconUrl}
-              alt={props.data.description}
-              className="float-left"
-            />
+            <div className="float-left">
+              <WeatherIcon />
+            </div>
             <div className="float-left">
               <span className="temperature">
                 {Math.round(props.data.temperature)}
